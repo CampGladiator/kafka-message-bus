24f2598c10f1dbd37cb9d09e38f1ea159db31b30
Eduardo Cunha <hey@drgmr.com>
2019-04-08 13:45:11 -0300

    Bump library version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule KafkaMessageBus.Mixfile do
     [
       app: :kafka_message_bus,
       included_applications: included_applications(),
-      version: "4.0.0-rc.4",
+      version: "4.0.0-rc.5",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
