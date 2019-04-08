49141dfd838673a58768a985c0ee0a9b99b5faf0
Eduardo Cunha <hey@drgmr.com>
2019-04-08 11:09:08 -0300

    Add a producer utility macro

--- lib/kafka_message_bus.ex
+++ lib/kafka_message_bus.ex
@@ -4,4 +4,18 @@ defmodule KafkaMessageBus do
   def produce(data, key, resource, action, opts \\ []) do
     Producer.produce(data, key, resource, action, opts)
   end
+
+  defp producer(opts) do
+    {resource, opts} = Keyword.pop(opts, :resource)
+
+    quote do
+      defp produce(message, key, action) do
+        unquote(__MODULE__).produce(message, key, unquote(resource), action, unquote(opts))
+      end
+    end
+  end
+
+  defmacro __using__([{which, opts}]) do
+    apply(__MODULE__, which, [opts])
+  end
 end
